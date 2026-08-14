f9938af89d64ef9e8c43ac68296b7066928bc2f7
TSISabit <tariksaidulislam@gmail.com>
2026-08-14 23:31:33 +0600

    Tested Common Utility

--- Main.java
+++ Main.java
@@ -1,23 +1,29 @@
-import model.Doctor;
-import model.Patient;
+import util.FileManager;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Doctor doctor = new Doctor("D001", "Dr. Rahman", "rahman@queuecare.com", "1234", "01700000000", "Cardiologist", 1000);
 
-        Patient patient = new Patient("P001", "Sabit", "sabit@gmail.com", "1234", 22, "Male", "01800000000", "Dhaka");
+        String fileName = "data/test.txt";
+
+        FileManager.writeToFile(
+                fileName,
+                "QueueCare is working!\n"
+        );
+
+        FileManager.writeToFile(
+                fileName,
+                "File handling is ready.\n"
+        );
+
+        List<String> data = FileManager.readFromFile(fileName);
 
         System.out.println("===== QueueCare =====");
 
-        System.out.println("\nDoctor Information:");
-        System.out.println("Name: " + doctor.getName());
-        System.out.println("Specialization: " + doctor.getSpecialization());
-        System.out.println("Fee: " + doctor.getConsultationFee());
-
-        System.out.println("\nPatient Information:");
-        System.out.println("Name: " + patient.getName());
-        System.out.println("Age: " + patient.getAge());
-        System.out.println("Phone: " + patient.getPhone());
+        for (String line : data) {
+            System.out.println(line);
+        }
     }
 }
