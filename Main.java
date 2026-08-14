0698a618fe68f3d72f8f0b827d21b401a6aa44bd
TSISabit <tariksaidulislam@gmail.com>
2026-08-14 23:36:17 +0600

    Tested Doctor Service File

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-import util.FileManager;
+import model.Doctor;
+import service.DoctorService;
 
 import java.util.List;
 
@@ -6,24 +7,81 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String fileName = "data/test.txt";
+        DoctorService doctorService = new DoctorService();
 
-        FileManager.writeToFile(
-                fileName,
-                "QueueCare is working!\n"
+        // Add doctors
+        Doctor doctor1 = new Doctor(
+                "D001",
+                "Dr. Rahman",
+                "rahman@queuecare.com",
+                "1234",
+                "01700000001",
+                "Cardiologist",
+                1000
         );
 
-        FileManager.writeToFile(
-                fileName,
-                "File handling is ready.\n"
+        Doctor doctor2 = new Doctor(
+                "D002",
+                "Dr. Karim",
+                "karim@queuecare.com",
+                "1234",
+                "01700000002",
+                "Dermatologist",
+                800
         );
 
-        List<String> data = FileManager.readFromFile(fileName);
+        doctorService.addDoctor(doctor1);
+        doctorService.addDoctor(doctor2);
 
-        System.out.println("===== QueueCare =====");
 
-        for (String line : data) {
-            System.out.println(line);
+        // Display all doctors
+        System.out.println("===== ALL DOCTORS =====");
+
+        List<Doctor> doctors = doctorService.getAllDoctors();
+
+        for (Doctor doctor : doctors) {
+
+            System.out.println(
+                    doctor.getId() + " | " +
+                    doctor.getName() + " | " +
+                    doctor.getSpecialization() + " | " +
+                    doctor.getConsultationFee()
+            );
+        }
+
+
+        // Search doctor
+        System.out.println("\n===== SEARCH DOCTOR =====");
+
+        Doctor foundDoctor =
+                doctorService.findDoctorById("D002");
+
+        if (foundDoctor != null) {
+
+            System.out.println(
+                    "Doctor Found: " +
+                    foundDoctor.getName()
+            );
+
+        } else {
+
+            System.out.println("Doctor not found.");
+        }
+
+
+        // Search by specialization
+        System.out.println("\n===== CARDIOLOGISTS =====");
+
+        List<Doctor> cardiologists =
+                doctorService.findBySpecialization("Cardiologist");
+
+        for (Doctor doctor : cardiologists) {
+
+            System.out.println(
+                    doctor.getName() +
+                    " - " +
+                    doctor.getSpecialization()
+            );
         }
     }
 }
