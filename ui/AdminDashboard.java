994e1f6f7e5304fbd0f5a3c707d6ad68e6dab38c
TSISabit <tariksaidulislam@gmail.com>
2026-08-16 00:21:41 +0600

    Fixed Admin Dashboard

--- ui/AdminDashboard.java
+++ ui/AdminDashboard.java
@@ -88,10 +88,7 @@ public class AdminDashboard extends JFrame {
 
         doctorButton.addActionListener(e -> new DoctorManagementFrame());
 
-        patientButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(this,
-                    "Patient Management will be added later.");
-        });
+        patientButton.addActionListener(e -> new PatientManagementFrame());
 
         appointmentButton.addActionListener(e -> {
             JOptionPane.showMessageDialog(this,
